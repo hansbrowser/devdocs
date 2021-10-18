4406757ae6b283bdfd2a428753ce955b98a0925f
Hans Brouwer <Hans@ttt.nl>
2021-10-18 19:29:55 +0000

    fix

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,20 +3,26 @@ module.exports = {
 	title: 'Development Docs',
 	description: 'Development documentation.',
 	head: [
-		["link", { rel: "icon", href: `/logo-hb-dark.png` }]
+		["link", { rel: "icon", href: "/logo-hb-dark.png" }]
 	],
 	themeConfig: {
+		logo: '/logo-hb-dark.png',
 		nav: [
 			{ text: 'Home', link: '/' },
 			{ text: 'Dapper', link: '/dapper/' }
 		],
 		sidebar: [
 			{ text: 'Dapper', link: '/dapper/' },
+			{ text: 'DevOps', link: '/devops/' },
 			{ text: "Docker", link: "/docker/" },
 			{ text: 'Dotnet', link: '/dotnet/' },
 			{ text: 'Elastic', link: '/elastic/' },
 			{ text: 'IdentityServer', link: '/identityserver/' },
-			{ text: 'Javascript', link: '/javascript/' },
+			{ text: 'Javascript', link: '/javascript/', 
+			children: [
+				{ text : "Async Await", link:'/javascript/async-await'},
+				{ text : "Pro tips", link: '/javascript/pro-tips'}
+			] },
 			{ text: 'Mac', link: '/mac/' },
 			{ text: 'MsSql', link: '/mssql/' },
 			{ text: 'Npm', link: '/npm/' },
